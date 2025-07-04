960d45c508d5b3727c13027c551bbbf7c711d9e6
Fabio Azevedo <fabiostahlazevedo@gmail.com>
2025-07-04 18:29:32 -0300

    Adiciona estrutura inicial do projeto Clinica_Medica

---
+++ src/main/java/com/clinica/sitema/SitemaApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.sitema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinica/sitema/SitemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.sitema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
